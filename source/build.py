a6ee5af3348f2aaed0b8aec9476bfb36032f3478
Harã Heique dos Santos <heikacademicos@hotmail.com>
2019-05-20 04:55:47 -0300

    Deleção da pasta imagens e modificações no código

--- source/build.py
+++ source/build.py
@@ -8,12 +8,11 @@ def main():
     parserMEL: LarkParserMEL = LarkParserMEL()
 
     while True:
-        inputExpression: str = input("Enter your math expression: ")
+        inputExpression: str = input("\nEnter your math expression: ")
         isValidExpr: bool = parserMEL.checkExpression(inputExpression)
         strIsValidExpr: str = "valid" if isValidExpr else "invalid"
         
         print("Expression {0} is {1}.".format(inputExpression, strIsValidExpr))
-        print()
 
     return 0
 
---
+++ source/trab2.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt install python3-pip
+pip3 install lark-parser
+python3 build.py
